4a8326ac9a9abc20f7046cda87832f5729a9fe57
charudutta <restrictedcharudutta@gmail.com>
2023-10-03 23:35:50 +0530

    Api gateway in GO added

---
+++ ApiGatewayGo/main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"net/http"
+	"os"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	port := os.Getenv("PORT")
+
+	if port == "" {
+		port = ":9000"
+	}
+
+	router := gin.New()
+	router.Use(gin.Logger())
+
+	// routes.authRoutes(router)
+
+	router.GET("/ping", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"message": "pong",
+		})
+	})
+
+	router.Run(port)
+}
